ac4e8218c988a5bac6d92343235e8a919ceb37b8
JAKThaDev <jakthadev@Jds-MacBook-Pro.local>
2021-04-08 09:13:30 -0400

    have all imports ready to desgin the layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'; 
+// this is the file that supports the root component of our angular appication 
 
+// this is another declarator that we will incounter in angular. 
+// The component decarator takes the javascript object as a parameter and 
+// the selcetor arises from the app root.  
+// the template url points to a file, an HTML file that contains the template to the component. 
+// the template defines the view for the component.  
+// the styleurls file contain all the scss stlying that can be used for the component. 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
