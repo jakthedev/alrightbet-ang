7cd95772df443cb5dd1d145c6fb47b83809a2720
JAKThaDev <jakthadev@Jds-MacBook-Pro.local>
2021-04-08 11:40:34 -0400

    completed descrition of leagues class

---
+++ src/app/sports/shared/leagues.ts
@@ -0,0 +1,13 @@
+export class Leagues {
+    id: string, 
+    name: string;  
+    image: string; 
+    category: string; 
+    feature: boolean; 
+    label: string;  
+    description: string; 
+    price: string;
+
+
+
+}
